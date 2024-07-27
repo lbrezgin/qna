3ae4f500a0f097d7ced3764f8b7c7b4a6155061f
lbrezgin <lewickbrez@gmail.com>
2024-07-27 15:49:37 +0200

    user can delete answers link

--- spec/features/answer/link/destroy_links_spec.rb
+++ spec/features/answer/link/destroy_links_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
-feature 'User can destroy questions links', %q{
+feature 'User can destroy answers links', %q{
   In order to correct mistakes
-  As an question's author
+  As an answer's author
   I'd like to be able to destroy links
 } do
 
   given!(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given!(:link) { create(:link, linkable: question) }
+  given!(:answer) { create(:answer, question: question, user: user) }
+  given!(:link) { create(:link, linkable: answer) }
 
-  scenario 'User destroy link when edit question', js: true do
+  scenario 'User destroy link when edit answer', js: true do
     sign_in(user)
     visit question_path(question)
 
-    click_on 'Edit question'
-    within '.question-links-manage' do
+    click_on 'Edit'
+    within '.answer-links-manage' do
       click_on 'X'
     end
     click_on 'Save'
