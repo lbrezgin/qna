9fd2f170bf3bb87759d89647f3ffbf8c6ce2a046
lbrezgin <lewickbrez@gmail.com>
2024-11-11 22:48:02 +0200

    refactor: edits

--- spec/services/daily_digest_spec.rb
+++ spec/services/daily_digest_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DailyDigest do
-  let!(:questions) { create_list(:question, 3) }
+  let!(:users) { create_list(:user, 5) }
+  let!(:questions) { create_list(:question, 3, user: users.first) }
   let!(:titles) { questions.map(&:title) }
 
   it 'sends daily digest to all users' do
-    User.all.each { |user| expect(DailyDigestMailer).to receive(:digest).with(user, titles).and_call_original }
+    users.each { |user| expect(DailyDigestMailer).to receive(:digest).with(user, titles).and_call_original }
     subject.send_digest
   end
 end
